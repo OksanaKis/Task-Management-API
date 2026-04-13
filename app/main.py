fa3e7aafe1ea3318a42ff617c7bf39c9570ac747
OksanaKis <oksanakisetoja@gmail.com>
2026-04-13 11:03:29 -0700

    Add API metadata
    Add API metadata (title, description, version)

--- app/main.py
+++ app/main.py
@@ -3,7 +3,11 @@ from fastapi import FastAPI
 from app.router.auth import router as auth_router
 from app.router.tasks import router as tasks_router
 
-app = FastAPI()
+app = FastAPI(
+    title="Task Management API",
+    description="Backend API with JWT authentication, task ownership, PostgreSQL, Docker, and CI",
+    version="1.0.0"
+)
 
 app.include_router(auth_router)
 app.include_router(tasks_router)
